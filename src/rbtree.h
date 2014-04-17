168a7715ac03fb19072500bf72fe6f8d212c81ad
John Tsiombikas <nuclear@member.fsf.org>
2014-04-17 13:35:43 +0300

    readme, copying, copyright headers

--- src/rbtree.h
+++ src/rbtree.h
@@ -1,3 +1,10 @@
+/*
+rbtree - simple balanced binary search tree (red-black tree) library.
+Copyright (C) 2011-2014  John Tsiombikas <nuclear@member.fsf.org>
+
+rbtree is free software, feel free to use, modify, and redistribute it, under
+the terms of the 3-clause BSD license. See COPYING for details.
+ */
 #ifndef RBTREE_H_
 #define RBTREE_H_
 
